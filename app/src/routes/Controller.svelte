eeb573b90a3850c2d1be0863d87526cb2a6051f7
Rune Harlyk <rune@harlyk.dk>
2023-07-12 15:30:39 +0200

    🕹 Adds controller page

---
+++ app/src/routes/Controller.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Stream from '../components/Views/Stream.svelte';
+	import Controls from '../components/Controls.svelte';
+</script>
+
+<div class="flex justify-center items-center w-full h-full">
+	<Stream />
+	<Controls />
+</div>
