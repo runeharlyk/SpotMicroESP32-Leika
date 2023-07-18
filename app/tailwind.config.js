39588c04fdedafd9cebaa54f70273b9d0dfdbaf8
Rune Harlyk <rune@harlyk.dk>
2023-07-18 17:52:14 +0200

    🎃 Adds darktheme

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{html,js,ts,svelte}'],
 	theme: {
-		extend: {}
+        extend: {
+          colors: {
+            'primary': '#6200EE',
+            'primary-variant': '#3700B3',
+            'secondary': '#3700B3',
+            'secondary-variant': '#3700B3',
+            'background': '#1e1e1e',
+            'surface': '#2c2c2c',
+            'error': '#B00020',
+            'on-primary': '#FFFFFF',
+            'on-secondary': '#FFFFFF',
+            'on-background': '#FFFFFF',
+            'on-surface': '#FFFFFF',
+            'on-error': '#FFFFFF',
+          }
+        },
 	},
 	plugins: []
 };
