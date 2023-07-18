60796e3b3c396f43affdde3711d844e0ed06f67f
Rune Harlyk <rune@harlyk.dk>
2023-07-18 17:56:17 +0200

    🐿 Renames store same

--- app/src/routes/SystemHealth.svelte
+++ app/src/routes/SystemHealth.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
-	import { data, socket } from '../lib/socket';
+	import { dataBuffer, socket } from '../lib/socket';
 	import { humanFileSize } from '../lib/utils';
 
 	let buf = new Uint8Array(2);
@@ -20,15 +20,15 @@
 <div class="p-10 flex gap-4">
 	<div class="bg-slate-600 rounded-md p-2 drop-shadow-lg">
 		<b>Heap allocation:</b>
-		<div class="flex gap-2"><span>Total free:</span>{humanFileSize($data[8])}</div>
-		<div class="flex gap-2"><span>Max free block:</span>{humanFileSize($data[12])}</div>
-		<div class="flex gap-2"><span>Min:</span>{humanFileSize($data[10])}</div>
+		<div class="flex gap-2"><span>Total free:</span>{humanFileSize($dataBuffer[8])}</div>
+		<div class="flex gap-2"><span>Max free block:</span>{humanFileSize($dataBuffer[12])}</div>
+		<div class="flex gap-2"><span>Min:</span>{humanFileSize($dataBuffer[10])}</div>
 	</div>
 
 	<div class="bg-slate-600 rounded-md p-2 drop-shadow-lg">
 		<b>PSRam allocation:</b>
-		<div class="flex gap-2"><span>Free</span>{humanFileSize($data[9])}</div>
-		<div class="flex gap-2"><span>Min:</span>{humanFileSize($data[11])}</div>
-		<div class="flex gap-2"><span>Max block:</span>{humanFileSize($data[13])}</div>
+		<div class="flex gap-2"><span>Free</span>{humanFileSize($dataBuffer[9])}</div>
+		<div class="flex gap-2"><span>Min:</span>{humanFileSize($dataBuffer[11])}</div>
+		<div class="flex gap-2"><span>Max block:</span>{humanFileSize($dataBuffer[13])}</div>
 	</div>
 </div>
