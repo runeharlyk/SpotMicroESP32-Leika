564f6fad1d2a12c9afd1835dff7f0a851777cf8f
Rune Harlyk <rune@harlyk.dk>
2023-08-02 22:01:07 +0200

    🧼 Adds vite-plugin-compression

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte(), viteSingleFile()],
+	plugins: [svelte(), viteSingleFile(), viteCompression({deleteOriginFile: true})],
 	build: {
 		outDir: '../data',
 		emptyOutDir: true
