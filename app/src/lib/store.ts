6d35e27991ad5388df30f7c77a0854102dd81383
Rune Harlyk <rune@harlyk.dk>
2024-02-03 17:59:07 +0100

    🎿 Adds initial controller data

--- app/src/lib/store.ts
+++ app/src/lib/store.ts
@@ -6,4 +6,4 @@ export const emulateModel = writable(true);
 
 export const input = writable({left:{x:0, y:0}, right:{x:0, y:0}, height:70, speed:0});
 
-export const outControllerData = writable(new Uint8Array(6));
+export const outControllerData = writable(new Uint8Array([0, 128, 128, 128, 128, 70, 0]));
