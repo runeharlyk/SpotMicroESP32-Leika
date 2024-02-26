46e0a47148fbbb6ce861ea859829e4dc2e8762df
Rune Harlyk <rune@harlyk.dk>
2024-02-26 15:35:15 +0100

    👍 Updates app unit test suite

--- app/test/specs/result.spec.ts
+++ app/test/specs/result.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { Result } from '../../src/lib/utilities';
+import { Result } from '../../src/lib/utilities/result';
 
 describe('Result', () => {
 	it('should create a success result correctly', () => {
