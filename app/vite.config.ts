ac1fca6285a1e79eea674630516435ab84cf9923
Rune Harlyk <rune@harlyk.dk>
2024-03-04 17:57:18 +0100

    🧟‍♀️ Update vite config to use env variables

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import viteCompression from 'vite-plugin-compression';
 import path from 'path';
-import 'dotenv/config';
-
-const embeddedBuild = process.env.VITE_EMBEDDED_BUILD == 'true';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		svelte(),
-		...(embeddedBuild ? [viteSingleFile(), viteCompression({ deleteOriginFile: true })] : [])
-	],
-	build: {
-		outDir: embeddedBuild ? '../data' : './build',
-		emptyOutDir: true
-	},
-	resolve: {
-		alias: {
-			$lib: path.resolve('./src/lib/'),
-			$components: path.resolve('./src/components'),
-			$utils: path.resolve('./src/utils'),
-			$stores: path.resolve('./src/stores')
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	const embeddedBuild = process.env.VITE_EMBEDDED_BUILD == 'true';
+
+	return defineConfig({
+		plugins: [
+			svelte(),
+			...(embeddedBuild ? [viteSingleFile(), viteCompression({ deleteOriginFile: true })] : [])
+		],
+		build: {
+			outDir: embeddedBuild ? '../esp32/data' : './build',
+			emptyOutDir: true
+		},
+		resolve: {
+			alias: {
+				$lib: path.resolve('./src/lib/'),
+				$components: path.resolve('./src/components'),
+				$utils: path.resolve('./src/utils'),
+				$stores: path.resolve('./src/stores')
+			}
 		}
-	}
-});
+	});
+};
