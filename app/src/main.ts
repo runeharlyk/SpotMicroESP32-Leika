bdd828b5c59be45e7f326dbe51d0fd69609ecbf1
Rune Harlyk <rune@harlyk.dk>
2024-03-17 22:01:54 +0100

    🍎 Makes app installable

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,12 @@ import './app.css';
 import './index.css';
 import App from './App.svelte';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js');
+	});
+}
+
 const app = new App({
 	target: document.getElementById('app') as HTMLElement
 });
