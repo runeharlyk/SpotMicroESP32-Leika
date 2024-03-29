180fea0f25e1389d550d5ec139729b3e8079367e
Rune Harlyk <rune@harlyk.dk>
2024-03-29 21:15:38 +0100

    Adds actuator service to sync angles

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,18 +1,26 @@
 #include <ESP32SvelteKit.h>
+#include <ActuatorStateService.h>
 #include <PsychicHttpServer.h>
 
 #define SERIAL_BAUD_RATE 115200
 
-PsychicHttpServer server;
+DRAM_ATTR PsychicHttpServer server;
 
-ESP32SvelteKit esp32sveltekit(&server, 120);
+DRAM_ATTR ESP32SvelteKit esp32sveltekit(&server, 120);
 
-void setup() {
+ActuatorStateService actuatorStateService = ActuatorStateService(&server, esp32sveltekit.getSecurityManager());
+
+
+void setup()
+{
     Serial.begin(SERIAL_BAUD_RATE);
 
     esp32sveltekit.begin();
+
+    actuatorStateService.begin();
 }
 
-void loop() {
+void loop()
+{
     vTaskDelete(NULL);
 }
