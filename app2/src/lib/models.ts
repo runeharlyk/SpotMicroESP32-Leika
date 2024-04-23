48e96d57758870f3677838af1f282f9c96028eb9
Rune Harlyk <rune@harlyk.dk>
2024-04-23 21:59:19 +0200

    Moves the MQTT types to models

--- app2/src/lib/models.ts
+++ app2/src/lib/models.ts
@@ -20,3 +20,20 @@ export type LogData = {
 };
 
 export type WebSocketJsonMsg = AnglesData | LogData;
+
+export type MQTTStatus = {
+	enabled: boolean;
+	connected: boolean;
+	client_id: string;
+	last_error: string;
+};
+
+export type MQTTSettings = {
+	enabled: boolean;
+	uri: string;
+	username: string;
+	password: string;
+	client_id: string;
+	keep_alive: number;
+	clean_session: boolean;
+};
