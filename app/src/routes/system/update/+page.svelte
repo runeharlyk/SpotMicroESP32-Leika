fd07baf2eea47db4bbbfdeb8f77ede11200fe071
Rune Harlyk <rune@harlyk.dk>
2024-05-02 18:43:34 +0200

    🧹 Removes unused svelte properties

--- app/src/routes/system/update/+page.svelte
+++ app/src/routes/system/update/+page.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import UploadFirmware from './UploadFirmware.svelte';
 	import GithubFirmwareManager from './GithubFirmwareManager.svelte';
 	import { user } from '$lib/stores/user';
 	import { page } from '$app/stores';
-
-	export let data: PageData;
 </script>
 
-<div
-	class="mx-0 my-1 flex flex-col space-y-4
-     sm:mx-8 sm:my-8"
->
+<div class="mx-0 my-1 flex flex-col space-y-4 sm:mx-8 sm:my-8">
 	{#if $page.data.features.download_firmware && (!$page.data.features.security || $user.admin)}
 		<GithubFirmwareManager />
 	{/if}
