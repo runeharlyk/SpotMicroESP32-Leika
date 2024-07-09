cee796c705f23d5adc11fe228700ff12474978f7
Rune Harlyk <rune@harlyk.dk>
2024-07-09 20:07:32 +0200

    🪄 Formats JsonUtils

--- esp32/lib/ESP32-sveltekit/JsonUtils.h
+++ esp32/lib/ESP32-sveltekit/JsonUtils.h
@@ -19,29 +19,22 @@
 #include <IPUtils.h>
 #include <ArduinoJson.h>
 
-class JsonUtils
-{
-public:
-    static void readIP(JsonObject &root, const String &key, IPAddress &ip, const String &def)
-    {
+class JsonUtils {
+  public:
+    static void readIP(JsonObject &root, const String &key, IPAddress &ip, const String &def) {
         IPAddress defaultIp = {};
-        if (!defaultIp.fromString(def))
-        {
+        if (!defaultIp.fromString(def)) {
             defaultIp = INADDR_NONE;
         }
         readIP(root, key, ip, defaultIp);
     }
-    static void readIP(JsonObject &root, const String &key, IPAddress &ip, const IPAddress &defaultIp = INADDR_NONE)
-    {
-        if (!root[key].is<String>() || !ip.fromString(root[key].as<String>()))
-        {
+    static void readIP(JsonObject &root, const String &key, IPAddress &ip, const IPAddress &defaultIp = INADDR_NONE) {
+        if (!root[key].is<String>() || !ip.fromString(root[key].as<String>())) {
             ip = defaultIp;
         }
     }
-    static void writeIP(JsonObject &root, const String &key, const IPAddress &ip)
-    {
-        if (IPUtils::isSet(ip))
-        {
+    static void writeIP(JsonObject &root, const String &key, const IPAddress &ip) {
+        if (IPUtils::isSet(ip)) {
             root[key] = ip.toString();
         }
     }
