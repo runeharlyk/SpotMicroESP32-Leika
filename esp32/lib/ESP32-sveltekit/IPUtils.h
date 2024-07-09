00e48b48901f89aac0ddf233b69a42f7a682ff94
Rune Harlyk <rune@harlyk.dk>
2024-07-09 20:07:19 +0200

    🪄 Formats IPUtils

--- esp32/lib/ESP32-sveltekit/IPUtils.h
+++ esp32/lib/ESP32-sveltekit/IPUtils.h
@@ -19,17 +19,10 @@
 
 const IPAddress IP_NOT_SET = IPAddress(INADDR_NONE);
 
-class IPUtils
-{
-public:
-    static bool isSet(const IPAddress &ip)
-    {
-        return ip != IP_NOT_SET;
-    }
-    static bool isNotSet(const IPAddress &ip)
-    {
-        return ip == IP_NOT_SET;
-    }
+class IPUtils {
+  public:
+    static bool isSet(const IPAddress &ip) { return ip != IP_NOT_SET; }
+    static bool isNotSet(const IPAddress &ip) { return ip == IP_NOT_SET; }
 };
 
 #endif // end IPUtils_h
