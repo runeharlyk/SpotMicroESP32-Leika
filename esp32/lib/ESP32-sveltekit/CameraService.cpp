9d6c537d596444cfb3e720538b370e9c3989d243
Rune Harlyk <rune@harlyk.dk>
2024-07-11 21:27:42 +0200

    💊 Updates camera task priority

--- esp32/lib/ESP32-sveltekit/CameraService.cpp
+++ esp32/lib/ESP32-sveltekit/CameraService.cpp
@@ -155,7 +155,7 @@ void streamTask(void *pv) {
 }
 
 esp_err_t CameraService::cameraStream(PsychicRequest *request) {
-    _taskManager->createTask(streamTask, "Stream client task", 4096, request, 0);
+    _taskManager->createTask(streamTask, "Stream client task", 4096, request, 4);
     vTaskDelay(pdMS_TO_TICKS(100));
     return ESP_OK;
 }
