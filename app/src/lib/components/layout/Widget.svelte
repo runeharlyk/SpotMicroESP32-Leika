00c56a2d687e017d96e447e51737e697d7dea3a8
Rune Harlyk <rune@harlyk.dk>
2024-08-18 02:07:41 +0200

    🎚️ Expands layout manager with widget sizing

--- app/src/lib/components/layout/Widget.svelte
+++ app/src/lib/components/layout/Widget.svelte
@@ -1,7 +1,10 @@
-<script>
-    export let size = 1;
+<script lang="ts">
+	import type { SizeUnit } from "./LayoutManager";
+
+    export let size = -1;
+    export let sizeUnit:SizeUnit = '%';
 </script>
 
-<div class="box-border overflow-hidden min-w-96" style="flex: {size}">
+<div class="box-border overflow-hidden min-w-96 "class:flex-1={size === -1}>
     <slot></slot>
 </div>
