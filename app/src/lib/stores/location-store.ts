cd3ad93196003270216c1797cbf47d98aa3eb65b
Rune Harlyk <rune@harlyk.dk>
2024-08-24 20:59:54 +0200

    🍁 Updates location store

--- app/src/lib/stores/location-store.ts
+++ app/src/lib/stores/location-store.ts
@@ -1,3 +1,5 @@
 import { persistentStore } from '$lib/utilities';
+import { writable } from 'svelte/store';
+import appEnv from 'app-env';
 
-export const location = persistentStore('location', '/');
+export const location = appEnv.VITE_USE_HOST_NAME ? writable('') : persistentStore('location', '');
