16e653afa8e01e193aaf0075f71dbe23eeadf707
Rune Harlyk <rune@harlyk.dk>
2024-08-24 21:04:30 +0200

    🍭 Updates the persistent store to save onload

--- app/src/lib/utilities/svelte-utilities.ts
+++ app/src/lib/utilities/svelte-utilities.ts
@@ -4,11 +4,13 @@ import { browser } from '$app/environment';
 export const persistentStore = <T>(key: string, initialValue: T) => {
 	const savedValue = browser ? localStorage.getItem(key) : null;
 	const data: T = savedValue !== null ? JSON.parse(savedValue) : initialValue;
-	const store = writable<T>(data);
+	const store = writable<T>();
 
-	store.subscribe((value) => {
+	store.subscribe(value => {
 		if (browser) localStorage.setItem(key, JSON.stringify(value));
 	});
+
+	store.set(data);
 
 	return store;
 };
