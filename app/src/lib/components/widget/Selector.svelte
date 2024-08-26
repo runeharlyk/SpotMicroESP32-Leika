539600b0d3184f1a1be01b43273d79590099054c
Rune Harlyk <rune@harlyk.dk>
2024-08-26 19:59:45 +0200

    🗽 Makes selector sm by default

--- app/src/lib/components/widget/Selector.svelte
+++ app/src/lib/components/widget/Selector.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-    export let options: string[] = [];
-    export let selectedOption: string = '';
-  </script>
-  
-  <select bind:value={selectedOption} on:change class="select select-bordered sm:select-sm max-w-xs {$$restProps.class || ''}">
-    {#each options as option}
-      <option value={option}>{option}</option>
-    {/each}
-  </select>
+	export let options: string[] = [];
+	export let selectedOption: string = '';
+</script>
+
+<select
+	bind:value={selectedOption}
+	on:change
+	class="select select-bordered select-sm lg:select-md max-w-xs {$$restProps.class || ''}"
+>
+	{#each options as option}
+		<option value={option}>{option}</option>
+	{/each}
+</select>
