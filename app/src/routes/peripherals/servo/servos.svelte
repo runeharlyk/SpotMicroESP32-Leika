21cf3c961e733c1e0092f665fa446768a6d42475
Rune Harlyk <rune@harlyk.dk>
2024-08-29 00:16:23 +0200

    🚝 Adds option to control servo single or all using pwm

--- app/src/routes/peripherals/servo/servos.svelte
+++ app/src/routes/peripherals/servo/servos.svelte
@@ -1,58 +1,94 @@
 <script lang="ts">
-	import SettingsCard from '$lib/components/SettingsCard.svelte';
-	import type { ServoConfiguration, Servo } from '$lib/types/models';
-	import ServoController from './servo.svelte';
-	import Spinner from '$lib/components/Spinner.svelte';
+    import SettingsCard from '$lib/components/SettingsCard.svelte';
+    import type { ServoConfiguration, Servo } from '$lib/types/models';
+    import ServoController from './servo.svelte';
+    import Spinner from '$lib/components/Spinner.svelte';
 
-	import { socket } from '$lib/stores';
-	import { onDestroy, onMount } from 'svelte';
-	import { throttler as Throttler } from '$lib/utilities';
-	import { MotorOutline } from '$lib/components/icons';
+    import { socket } from '$lib/stores';
+    import { onDestroy, onMount } from 'svelte';
+    import { throttler as Throttler } from '$lib/utilities';
+    import { MotorOutline } from '$lib/components/icons';
+    import Selector from '$lib/components/widget/Selector.svelte';
 
-	let isLoading = false;
+    let isLoading = false;
 
-    let active = false
+    let active = false;
 
-    let servoId = 0
+    let servoId = 0;
 
-    const throttler = new Throttler()
+    const throttler = new Throttler();
 
-    const sweep = (event:any) => {
+    const states = ['Deactivated', 'Active', 'Single_PWM', 'All_PWM'] as const;
+
+    enum ServoState {
+        Deactivated,
+        Active,
+        Single_PWM,
+        All_PWM
+    }
+
+    type States = (typeof ServoState)[number];
+
+    let state: States = 'Deactivated';
+
+    const sweep = (event: any) => {
         let channel = event.detail.channel;
-        socket.sendEvent('servoConfiguration', {servos:[{channel, sweep: true}]});
-    };
-
-    const activateServo = (event:any) => {
-        socket.sendEvent('servoState', {'active':1});
-    };
-
-    const deactivateServo = (event:any) => {
-        socket.sendEvent('servoState', {'active':0});
+        socket.sendEvent('servoConfiguration', { servos: [{ channel, sweep: true }] });
     };
 
     let pwm = 306;
+    let angle = 90;
+    let controlType = 'PWM';
 
-    const updatePWM = () => {
+    const updatePosition = () => {
         throttler.throttle(() => {
-            socket.sendEvent('servoPWM', {servo_id:servoId, pwm});
-        }, 10)
-    }
+            socket.sendEvent(`servo${controlType}`, { servo_id: servoId, pwm });
+        }, 10);
+    };
+
+    const updateState = () => {
+        socket.sendEvent('servoState', { data: states.indexOf(state) });
+    };
 </script>
 
 <SettingsCard collapsible={false}>
-	<MotorOutline slot="icon" class="lex-shrink-0 mr-2 h-6 w-6 self-end" />
-	<span slot="title">Servo</span>
-    <input type="range" min="200" max="400" bind:value={pwm} on:input={updatePWM} class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+    <MotorOutline slot="icon" class="lex-shrink-0 mr-2 h-6 w-6 self-end" />
+    <span slot="title">Servo</span>
 
-	{#if isLoading}
-		<Spinner />
-	{:else}
-		<div class="flex flex-col">
-			<h2 class="text-lg">General servo configuration</h2>
-            <span class="flex items-center gap-2">
-                <label for="servoId">Servo active{servoId}</label>
-                <input type="checkbox" class="toggle" bind:checked={active} on:change={active ? deactivateServo : activateServo}>
-            </span>
+    <div class="flex gap-4 items-center">
+        State
+        <Selector bind:selectedOption={state} options={states} on:change={updateState} />
+    </div>
+    {#if state === 'Single_angle' || state === 'Single_PWM'}
+        <div>
+            Servo number
+            <input class="input" type="number" bind:value={servoId} />
         </div>
-	{/if}
+    {/if}
+
+    <div class="flex items-center">
+        <span class="w-32">Control option:</span>
+        <Selector bind:selectedOption={controlType} options={['PWM', 'Degree']} />
+    </div>
+    {#if controlType === 'PWM'}
+        <input
+            type="range"
+            min="100"
+            max="800"
+            bind:value={pwm}
+            on:input={updatePosition}
+            class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        />
+        <div>{pwm} pwm</div>
+    {:else}
+        <input
+            type="range"
+            min="0"
+            max="180"
+            bind:value={angle}
+            on:input={updatePosition}
+            class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        />
+        <div>{angle} degrees</div>
+    {/if}
 </SettingsCard>
--- esp32/lib/ESP32-sveltekit/ServoController.h
+++ esp32/lib/ESP32-sveltekit/ServoController.h
@@ -10,8 +10,11 @@
 #include <MathUtils.h>
 #include <Timing.h>
 
-#define EVENT_SERVO_CONFIGURATION_SETTINGS "servoPWM"
+#define EVENT_SERVO_ANGLE "servoAngle"
+#define EVENT_SERVO_PWM "servoPWM"
 #define EVENT_SERVO_STATE "servoState"
+
+enum class SERVO_STATE { DEACTIVATED, ACTIVE, SINGLE_PWM, ALL_PWM };
 
 class ServoController {
   public:
@@ -20,22 +23,36 @@ class ServoController {
         : _server(server), _securityManager(securityManager), _peripherals(peripherals), _socket(socket) {}
 
     void begin() {
-        _socket->onEvent(EVENT_SERVO_CONFIGURATION_SETTINGS,
-                         [&](JsonObject &root, int originId) { servoEvent(root, originId); });
-        _socket->onEvent(EVENT_SERVO_STATE, [&](JsonObject &root, int originId) { stateUpdate(root, originId); });
+        _socket->onEvent(EVENT_SERVO_ANGLE, [&](JsonObject &root, int originId) { angleEvent(root, originId); });
+        _socket->onEvent(EVENT_SERVO_PWM, [&](JsonObject &root, int originId) { pwmEvent(root, originId); });
+        _socket->onEvent(EVENT_SERVO_STATE, [&](JsonObject &root, int originId) { handleMode(root, originId); });
     }
 
-    void stateUpdate(JsonObject &root, int originId) {
-        bool active = root["active"].as<bool>();
-        ESP_LOGI("SERVOCONTROLLER", "Setting state %d", active);
-        active ? activate() : deactivate();
+    void handleMode(JsonObject &root, int originId) {
+        state = (SERVO_STATE)root["data"].as<int>();
+        state == SERVO_STATE::DEACTIVATED ? deactivate() : activate();
+        ESP_LOGI("SERVO_CONTROLLER", "Updates state");
     }
 
-    void servoEvent(JsonObject &root, int originId) {
+    void angleEvent(JsonObject &root, int originId) {
         uint8_t servo_id = root["servo_id"];
+        uint16_t angle = root["angle"];
+        angles[servo_id] = angle;
+        ESP_LOGI("SERVO_CONTROLLER", "Setting servo %d to %d degree", servo_id, angle);
+    }
+
+    void pwmEvent(JsonObject &root, int originId) {
         uint16_t pwm = root["pwm"];
-        center[servo_id] = pwm;
-        ESP_LOGI("SERVO_CONTROLLER", "Setting servo %d to %d", servo_id, pwm);
+        if (state == SERVO_STATE::SINGLE_PWM) {
+            uint8_t servo_id = root["servo_id"];
+            pwm_signal[servo_id] = pwm;
+            ESP_LOGI("SERVO_CONTROLLER", "Setting servo %d to %d", servo_id, pwm);
+        } else if (state == SERVO_STATE::ALL_PWM) {
+            for (int i = 0; i < 12; i++) {
+                pwm_signal[i] = pwm;
+            }
+            ESP_LOGI("SERVO_CONTROLLER", "Setting all servos to %d", pwm);
+        }
     }
 
     void syncAngles(const String &originId) {
@@ -50,8 +67,6 @@ class ServoController {
 
     void activate() { _peripherals->pcaActivate(); }
 
-    void updateActiveState() { is_active ? activate() : deactivate(); }
-
     void setAngles(float new_angles[12]) {
         for (int i = 0; i < 12; i++) {
             target_angles[i] = new_angles[i];
@@ -60,14 +75,18 @@ class ServoController {
 
     void updateServoState() {
         for (int i = 0; i < 12; i++) {
-            angles[i] = lerp(angles[i], target_angles[i], 0.2);
-            float angle = dir[i] * angles[i] + center_angle_deg[i];
-            uint16_t pwm = angle * servo_conversion[i] + center[i];
-            if (pwm < 125 || pwm > 600) {
-                ESP_LOGE("ServoController", "Servo %d, Invalid PWM value %d", i, pwm);
-                continue;
+            if (state == SERVO_STATE::SINGLE_PWM || state == SERVO_STATE::ALL_PWM) {
+                _peripherals->pcaWrite(i, pwm_signal[i]);
+            } else {
+                angles[i] = lerp(angles[i], target_angles[i], 0.2);
+                float angle = dir[i] * angles[i] + center_angle_deg[i];
+                uint16_t pwm = angle * servo_conversion[i] + center[i];
+                if (pwm < 125 || pwm > 600) {
+                    ESP_LOGE("ServoController", "Servo %d, Invalid PWM value %d", i, pwm);
+                    continue;
+                }
+                _peripherals->pcaWrite(i, pwm);
             }
-            _peripherals->pcaWrite(i, pwm);
         }
     }
 
@@ -81,11 +100,12 @@ class ServoController {
     Peripherals *_peripherals;
     EventSocket *_socket;
 
-    bool is_active {true};
+    SERVO_STATE state {SERVO_STATE::DEACTIVATED};
     constexpr static int ServoInterval = 2;
 
-    float center[12] = {306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306};
-    float dir[12] = {-1, 1, 1, -1, -1, -1, 1, 1, 1, 1, -1, -1};
+    uint16_t center[12] = {306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306};
+    uint16_t pwm_signal[12] = {306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306, 306};
+    int8_t dir[12] = {-1, 1, 1, -1, -1, -1, 1, 1, 1, 1, -1, -1};
     float center_angle_deg[12] = {0, -45, 90, 0, 45, -90, 0, -45, 90, 0, 45, -90};
 
     float angles[12] = {0, 90, -145, 0, 90, -145, 0, 90, -145, 0, 90, -145};
