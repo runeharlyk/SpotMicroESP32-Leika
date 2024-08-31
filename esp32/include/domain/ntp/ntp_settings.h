6bde606ed5fe8db7e24b417166267c802345c7e9
Rune Harlyk <rune@harlyk.dk>
2024-08-31 23:37:50 +0200

    ⌚ Adds ntp model

---
+++ esp32/include/domain/ntp/ntp_settings.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <domain/state_result.h>
+
+#ifndef FACTORY_NTP_ENABLED
+#define FACTORY_NTP_ENABLED true
+#endif
+
+#ifndef FACTORY_NTP_TIME_ZONE_LABEL
+#define FACTORY_NTP_TIME_ZONE_LABEL "Europe/London"
+#endif
+
+#ifndef FACTORY_NTP_TIME_ZONE_FORMAT
+#define FACTORY_NTP_TIME_ZONE_FORMAT "GMT0BST,M3.5.0/1,M10.5.0"
+#endif
+
+#ifndef FACTORY_NTP_SERVER
+#define FACTORY_NTP_SERVER "time.google.com"
+#endif
+
+class NTPSettings {
+  public:
+    bool enabled;
+    String tzLabel;
+    String tzFormat;
+    String server;
+
+    static void read(NTPSettings &settings, JsonObject &root) {
+        root["enabled"] = settings.enabled;
+        root["server"] = settings.server;
+        root["tz_label"] = settings.tzLabel;
+        root["tz_format"] = settings.tzFormat;
+    }
+
+    static StateUpdateResult update(JsonObject &root, NTPSettings &settings) {
+        settings.enabled = root["enabled"] | FACTORY_NTP_ENABLED;
+        settings.server = root["server"] | FACTORY_NTP_SERVER;
+        settings.tzLabel = root["tz_label"] | FACTORY_NTP_TIME_ZONE_LABEL;
+        settings.tzFormat = root["tz_format"] | FACTORY_NTP_TIME_ZONE_FORMAT;
+        return StateUpdateResult::CHANGED;
+    }
+};
