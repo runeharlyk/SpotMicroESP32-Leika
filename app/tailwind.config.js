48d8b4f958311c8c0f2e70e271cf70c43a08700d
Rune Harlyk <rune@harlyk.dk>
2024-10-29 10:17:33 +0100

    💐 Updates tailwind config to import daisyui

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,29 +1,32 @@
+import daisyui from 'daisyui';
+import { light, dark } from 'daisyui/src/theming/themes';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,ts,svelte}'],
-	theme: {
-		extend: {}
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: [
-			{
-				light: {
-					...require('daisyui/src/theming/themes')['light'],
-					primary: '#00bfff',
-					accent: '#3c00ff',
-					'base-content': 'oklch(0.3 0.012 256)'
-				}
-			},
-			{
-				dark: {
-					...require('daisyui/src/theming/themes')['dark'],
-					primary: '#00bfff',
-					accent: '#3c00ff',
-					'base-content': 'white'
-				}
-			}
-		],
-		darkTheme: 'dark'
-	}
+    content: ['./src/**/*.{html,js,ts,svelte}'],
+    theme: {
+        extend: {}
+    },
+    plugins: [daisyui],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    ...light,
+                    primary: '#00bfff',
+                    accent: '#3c00ff',
+                    'base-content': 'oklch(0.3 0.012 256)'
+                }
+            },
+            {
+                dark: {
+                    ...dark,
+                    primary: '#00bfff',
+                    accent: '#3c00ff',
+                    'base-content': 'white'
+                }
+            }
+        ],
+        darkTheme: 'dark'
+    }
 };
