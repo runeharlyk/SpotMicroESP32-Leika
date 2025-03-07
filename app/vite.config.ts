9ff6dd7d4f9fddfef2e7cb587527a5181faa68c5
Rune Harlyk <rune@harlyk.dk>
2025-03-07 23:15:39 +0100

    🍇 Bumps vite version

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,30 +1,30 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import Icons from 'unplugin-icons/vite';
-import viteLittleFS from './vite-plugin-littlefs';
-import EnvCaster from '@niku/vite-env-caster';
-import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
+import viteLittleFS from './vite-plugin-littlefs'
+import EnvCaster from '@niku/vite-env-caster'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        sveltekit(),
-        Icons({
-            compiler: 'svelte'
-        }),
-        viteLittleFS(),
-        EnvCaster()
-    ],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://spot-micro.local/',
-                changeOrigin: true,
-                ws: true
-            }
-        }
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    Icons({
+      compiler: 'svelte'
+    }),
+    viteLittleFS(),
+    EnvCaster()
+  ],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://spot-micro.local/',
+        changeOrigin: true,
+        ws: true
+      }
     }
-});
+  }
+})
