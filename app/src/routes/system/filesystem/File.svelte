5a24038d688af8f3ffc2bdfc9a7e23ce1a598957
Rune Harlyk <rune@harlyk.dk>
2025-03-08 15:55:13 +0100

    📂 Fixes file system view

--- app/src/routes/system/filesystem/File.svelte
+++ app/src/routes/system/filesystem/File.svelte
@@ -6,6 +6,6 @@
 
 <!-- svelte-ignore a11y_interactive_supports_focus -->
 <!-- svelte-ignore a11y_click_events_have_key_events -->
-<span role="button" class="flex pl-4 gap-2 items-center" onclick={selected}>
+<span role="button" class="flex pl-4 gap-2 items-center" onclick={() => selected(name)}>
   <FileIcon />{name}
 </span>
