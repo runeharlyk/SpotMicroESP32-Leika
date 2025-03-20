b113a309427aa4497fa3e9ba11babc93caa4adb3
Rune Harlyk <rune@harlyk.dk>
2025-03-20 15:33:31 +0100

    🥷 Adds i2c configurator

--- app/src/routes/peripherals/i2c/i2c.svelte
+++ app/src/routes/peripherals/i2c/i2c.svelte
@@ -1,57 +1,81 @@
 <script lang="ts">
-    import SettingsCard from '$lib/components/SettingsCard.svelte';
-    import { onMount } from 'svelte';
-    import { socket } from '$lib/stores';
-    import type { I2CDevice } from '$lib/types/models';
-    import { Connection } from '$lib/components/icons';
+  import SettingsCard from '$lib/components/SettingsCard.svelte'
+  import { onMount } from 'svelte'
+  import { socket } from '$lib/stores'
+  import type { I2CDevice, PeripheralsConfiguration } from '$lib/types/models'
+  import { Cancel, Connection, Edit, Power, Router } from '$lib/components/icons'
+  import { modals } from 'svelte-modals'
+  import ConfirmDialog from '$lib/components/ConfirmDialog.svelte'
+  import Spinner from '$lib/components/Spinner.svelte'
+  import StatusItem from '$lib/components/StatusItem.svelte'
+  import I2CSetting from './i2cSetting.svelte'
 
-    const i2cDevices = [
-        { address: 30, part_number: 'HMC5883', name: '3-Axis Digital Compass/Magnetometer IC' },
-        { address: 64, part_number: 'PCA9685', name: '16-channel PWM driver default address' },
-        { address: 72, part_number: 'ADS1115', name: '4-channel 16-bit ADC' },
-        {
-            address: 104,
-            part_number: 'MPU6050',
-            name: 'Six-Axis (Gyro + Accelerometer) MEMS MotionTracking™ Devices'
-        },
-        { address: 119, part_number: 'BMP085', name: 'Temp/Barometric' }
-    ];
+  const i2cDevices = [
+    { address: 30, part_number: 'HMC5883', name: '3-Axis Digital Compass/Magnetometer IC' },
+    { address: 64, part_number: 'PCA9685', name: '16-channel PWM driver default address' },
+    { address: 72, part_number: 'ADS1115', name: '4-channel 16-bit ADC' },
+    {
+      address: 104,
+      part_number: 'MPU6050',
+      name: 'Six-Axis (Gyro + Accelerometer) MEMS MotionTracking™ Devices'
+    },
+    { address: 119, part_number: 'BMP085', name: 'Temp/Barometric' }
+  ]
 
-    let active_devices: I2CDevice[] = $state([]);
+  let active_devices: I2CDevice[] = $state([])
 
-    onMount(() => {
-        socket.on('i2cScan', handleScan);
-        socket.sendEvent('i2cScan', '');
-        return () => socket.off('i2cScan', handleScan);
-    });
+  let isLoading = $state(false)
 
-    const handleScan = (data: any) => {
-        active_devices = data.addresses.map(
-            (address: number) =>
-                i2cDevices.find(device => device.address === address) || {
-                    address,
-                    part_number: 'Unknown',
-                    name: 'Unknown'
-                }
-        );
-    };
+  onMount(() => {
+    socket.on('i2cScan', handleScan)
+    triggerScan()
+    return () => socket.off('i2cScan', handleScan)
+  })
+
+  const handleScan = (data: any) => {
+    active_devices = data.addresses.map(
+      (address: number) =>
+        i2cDevices.find(device => device.address === address) || {
+          address,
+          part_number: 'Unknown',
+          name: 'Unknown'
+        }
+    )
+    isLoading = false
+  }
+
+  const triggerScan = () => {
+    isLoading = true
+    socket.sendEvent('i2cScan', '')
+  }
 </script>
 
 <SettingsCard collapsible={false}>
-    {#snippet icon()}
-        <Connection  class="lex-shrink-0 mr-2 h-6 w-6 self-end" />
-    {/snippet}
-    {#snippet title()}
-        <span >I<sup>2</sup>C</span>
-    {/snippet}
+  {#snippet icon()}
+    <Connection class="lex-shrink-0 mr-2 h-6 w-6 self-end" />
+  {/snippet}
+  {#snippet title()}
+    <span>I<sup>2</sup>C</span>
+  {/snippet}
+  {#snippet right()}
+    <button class="btn btn-primary" onclick={triggerScan} disabled={isLoading}>
+      {#if isLoading}
+        <span class="loading loading-ring loading-xs"></span>
+      {:else}
+        Scan
+      {/if}
+    </button>
+  {/snippet}
 
-    <div class="grid">
-        {#if active_devices.length === 0}
-            <div>No I2C devices found</div>
-        {:else}
-            {#each active_devices as device}
-                <div>[{device.address.toString(16)}] {device.part_number} - {device.name}</div>
-            {/each}
-        {/if}
-    </div>
+  <I2CSetting />
+
+  <div class="grid">
+    {#if active_devices.length === 0}
+      <div>No I2C devices found</div>
+    {:else}
+      {#each active_devices as device}
+        <div>[{device.address.toString(16)}] {device.part_number} - {device.name}</div>
+      {/each}
+    {/if}
+  </div>
 </SettingsCard>
