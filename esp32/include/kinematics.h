9ceb7a9919e0d22543292674aff2994bb10fe63d
Rune Harlyk <rune@harlyk.dk>
2026-01-03 14:49:15 +0100

    ♻️ Fix pitch and roll units

--- esp32/include/kinematics.h
+++ esp32/include/kinematics.h
@@ -39,8 +39,8 @@ class KinConfig {
     };
 
     // Max constants
-    static constexpr float max_roll = 15 * DEG2RAD_F;
-    static constexpr float max_pitch = 15 * DEG2RAD_F;
+    static constexpr float max_roll = 20.0f;
+    static constexpr float max_pitch = 15.0f;
 
     static constexpr float max_body_shift_x = W / 3;
     static constexpr float max_body_shift_z = W / 3;
